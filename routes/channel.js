d3145ed98de0299a607108b6898b35e60810390e
William Martinez <w.martinez@costamar.com>
2015-04-11 22:51:25 -0500

    fixing some tab spaces

--- routes/channel.js
+++ routes/channel.js
@@ -2,27 +2,27 @@ var db = require('../lib/db');
 
 module.exports.findChannels = function (request, response) {
 	
-	db.findChannels(function (err, results) {
-		if (err) {
-			response.end(JSON.stringify({
-				success: false,
-				channels: []
-			}));
-		} else {
-			if (results.length > 0) {
-				response.end(JSON.stringify({
-					success: true,
-					channels: results
-				}));
-			}
-		}
-	});
+  db.findChannels(function (err, results) {
+    if (err) {
+      response.end(JSON.stringify({
+        success: false,
+        channels: []
+      }));
+    } else {
+      if (results.length > 0) {
+        response.end(JSON.stringify({
+          success: true,
+          channels: results
+        }));
+      }
+    }
+  });
 	
 };
 
 module.exports.saveChannel = function (request, response) {
 
-	db.saveChannel({
+  db.saveChannel({
       type: request.param('type'),
       user: request.param('username'),
       flight: request.param('flight'),
@@ -30,19 +30,19 @@ module.exports.saveChannel = function (request, response) {
     function(err, saved) {
       if(err) {
         response.end(JSON.stringify({
-					success: false,
-					message: 'Cannot create channel'
-				}));
+          success: false,
+          message: 'Cannot create channel'
+        }));
         return;
       }
       if(saved) {
         response.end(JSON.stringify({
-        	success: true
+          success: true
         }));
       } else {
         response.end(JSON.stringify({
-        	success: false,
-        	message: 'The channel already exists'
+          success: false,
+          message: 'The channel already exists'
         }));
       }
       return;
--- routes/user.js
+++ routes/user.js
@@ -3,18 +3,18 @@ var db = require('../lib/db')
 
 module.exports.saveUser = function (request, response) {
 
-	if (request.user == "undefined") {
-		response.end(JSON.stringify({
-			error: 'User is not defined'
-		}));
-		return;
-	}
+  if (request.user == "undefined") {
+    response.end(JSON.stringify({
+      error: 'User is not defined'
+    }));
+    return;
+  }
 
-	if (!validateUser(request.user)) {
-		response.end(JSON.stringify({
-			error: 'Some fields cannot be blank'
-		}));
-	}
+  if (!validateUser(request.user)) {
+    response.end(JSON.stringify({
+      error: 'Some fields cannot be blank'
+    }));
+  }
 	
   db.saveUser({
       username: request.param('username'),
@@ -24,19 +24,19 @@ module.exports.saveUser = function (request, response) {
     function(err, saved) {
       if(err) {
         response.end(JSON.stringify({
-					success: false,
-					message: 'Cannot create user'
-				}));
+          success: false,
+          message: 'Cannot create user'
+        }));
         return;
       }
       if(saved) {
         response.end(JSON.stringify({
-        	success: true
+          success: true
         }));
       } else {
         response.end(JSON.stringify({
-        	success: false,
-        	message: 'The user already exists'
+          success: false,
+          message: 'The user already exists'
         }));
       }
       return;
@@ -50,35 +50,37 @@ module.exports.findUserByUsername = function (request, response) {
   var username = request.params.username;
   
   db.findUsersByUsername(username, function (err, user) {
-  	if(err) {
+    
+    if(err) {
       response.end(JSON.stringify({
-      	error: 'Sorry, but something went wrong'
+        error: 'Sorry, but something went wrong'
       }));
       return;
     }
+    
     if(user === null) {
       response.end(JSON.stringify({
-      	success: false,
-      	message: 'User doesn`t exists',
-      	user: null
+        success: false,
+        message: 'User doesn`t exists',
+        user: null
       }));
       return;
     }
     else {
-    	response.end(JSON.stringify({
-    		success: true,
-    		user: user
-    	}));
+      response.end(JSON.stringify({
+        success: true,
+        user: user
+      }));
     }
-	});
+  });
 
 }
 
 var validateUser = function (user) {
-	for (var property in user) {
-		if (user[property] == null || user[property].length == 0) {
-			return false
-		}
-	}
-	return true;
+  for (var property in user) {
+    if (user[property] == null || user[property].length === 0) {
+      return false;
+    }
+  }
+  return true;
 }
